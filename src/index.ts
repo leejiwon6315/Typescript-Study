85e0d1833bd2ec1d6483bee7d5effb276690705d
leejiwon6315 <leejiwon6315@naver.com>
2021-01-30 01:05:58 +0900

    create block class for block chain

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
-const name = "jeewon",
-  age = 25,
-  gender = "male";
+import * as CryptoJS from "crypto-js";
 
-const sayHi = (name: string, age: number, gender?: string): string => {
-  // 변수들의 타입을 직접 지정해주기 + 함수가 반환할 타입을 설정
-  // ?는 선택적인 요소를 넣을 때 사용
-  return `name: ${name} age: ${age} gender: ${gender}`;
-};
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-console.log(sayHi(name, age));
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock = new Block(0, "63159599", "", "Hello", 123456);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+console.log(blockchain);
 
 export {};
